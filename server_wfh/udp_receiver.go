90ef76015a434838544d3247addf7847c083ac45
MarekaGit <martinespekarlsen@gmail.com>
2023-01-25 11:09:21 +0100

    send message go

--- server_wfh/udp_receiver.go
+++ server_wfh/udp_receiver.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"time"
 )
 
-func main() {
-	ServerConn, _ := net.ListenUDP("udp", &net.UDPAddr{IP: []byte{0, 0, 0, 0}, Port: 30000, Zone: ""})
+func receiver(port int) {
+	ServerConn, _ := net.ListenUDP("udp", &net.UDPAddr{IP: []byte{0, 0, 0, 0}, Port: port, Zone: ""})
 	defer ServerConn.Close()
 	buf := make([]byte, 1024)
 	for {
 		n, addr, _ := ServerConn.ReadFromUDP(buf)
 		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
 	}
+}
 
-	
+func send(message string) {
+	p := make([]byte, 2048)
+	conn, err := net.Dial("udp", "127.0.0.1:1234")
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Fprintf(conn, message)
+	time.Sleep(1 * time.Second)
+	_, err = bufio.NewReader(conn).Read(p)
+	if err == nil {
+		fmt.Printf("%s\n", p)
+	} else {
+		fmt.Printf("Some error %v\n", err)
+	}
+	conn.Close()
+}
+
+func main() {
+	receiver(30000)
+	send("Hi Server, How are you doing?")
 }
