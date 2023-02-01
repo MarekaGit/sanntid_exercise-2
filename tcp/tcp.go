50bedb3b4892cc648bf7b860c479870d60c5ff60
MarekaGit <martinespekarlsen@gmail.com>
2023-02-01 11:27:53 +0100

    tcp omtrent ferdig

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -1,6 +1,81 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	HOST = "10.100.23.11"
+	PORT = "34933"
+	TYPE = "tcp"
+)
+
+func receiver(port string) {
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+port)
+
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	// buffer to get data
+	received := make([]byte, 1024)
+	_, err = conn.Read(received)
+	if err != nil {
+		println("Read data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("Received message:", string(received))
+
+	conn.Close()
+}
+
+func send(message string, port string) {
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+port)
+
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("This is a message"))
+	if err != nil {
+		println("Write data failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("I Sent")
+	conn.Close()
+}
+
+func main() {
+	go receiver("20005")
+	send("Hi Server, How are you doing?", "20005")
+	//go receiver(34933)
+
+	time.Sleep(2 * time.Second)
+}
+
+//10.100.23.11:59300
+
+/*package main
+
+import (
 	"bytes"
 	"fmt"
 	"net"
@@ -15,7 +90,7 @@ func receiver(port int) {
 	reader := bytes.NewReader(buf)
 	for {
 		addr := ServerConn.Addr()
-		n, err := akseptert_tcp.ReadFrom(reader)
+		n, err := akseptert_tcp.Read(reader)
 		if err != nil {
 			panic("panic")
 		}
@@ -24,7 +99,7 @@ func receiver(port int) {
 }
 
 func send(message string, port int) {
-	Conn, _ := net.DialTCP("tcp", nil, &net.TCPAddr{IP: []byte{10, 100, 23, 241}, Port: port, Zone: ""})
+	Conn, _ := net.DialTCP("tcp", nil, &net.TCPAddr{IP: []byte{10, 100, 23, 11}, Port: port, Zone: ""})
 	defer Conn.Close()
 	time.Sleep(1 * time.Second)
 	Conn.Write([]byte(message))
@@ -32,11 +107,11 @@ func send(message string, port int) {
 }
 
 func main() {
-	//go receiver(20005)
-	//send("Hi Server, How are you doing?", 20005)
-	go receiver(34933)
+	go receiver(20005)
+	send("Hi Server, How are you doing?", 20005)
+	//go receiver(34933)
 
 	time.Sleep(2 * time.Second)
-}
+}*/
 
-//10.100.23.241:45123
+//10.100.23.11:59300
